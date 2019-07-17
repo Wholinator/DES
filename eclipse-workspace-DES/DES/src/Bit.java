50911c5510b256f01eb7cb6920f412d717156ce5
Wholey <MWholey@dsthealth.com>
2019-07-17 15:08:43 -0500

    Created Bit and Bitstring classes, added conversion from bytearray to bitarray

---
+++ eclipse-workspace-DES/DES/src/Bit.java
@@ -0,0 +1,21 @@
+
+public class Bit {
+	private int value;
+	
+	public Bit(int value) {
+		setValue(value);
+	}
+	
+	public Bit() {
+		setValue(0);
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+}
